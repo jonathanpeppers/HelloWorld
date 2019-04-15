77a603f7676af3715e2f96ce14939b88ef833f91
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-04-15 15:13:50 -0500

    HelloForms -> new XF Shell template

---
+++ HelloForms/HelloForms/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using HelloForms.Models;
+using HelloForms.ViewModels;
+
+namespace HelloForms.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+		public ItemDetailPage (ItemDetailViewModel viewModel)
+		{
+			InitializeComponent ();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+
+		public ItemDetailPage ()
+		{
+			InitializeComponent ();
+
+			var item = new Item {
+				Text = "Item 1",
+				Description = "This is an item description."
+			};
+
+			viewModel = new ItemDetailViewModel (item);
+			BindingContext = viewModel;
+		}
+	}
+}
